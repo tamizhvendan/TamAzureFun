a03e6dc86f6509c0a355d6ed93b2099f9cba7ec5
Tamizhvendan S <tamizh88@gmail.com>
2016-09-11 18:07:48 +0530

    trying with post

--- HelloSuave/run.fsx
+++ HelloSuave/run.fsx
@@ -30,7 +30,7 @@ let toHttpResponseMessage (httpResult : HttpResult) =
   res.Content <- new ByteArrayContent(content httpResult.content)
   res
 
-let app = GET >=> OK "hello"
+let app = POST >=> OK "hello"
 
 let Run (req : HttpRequestMessage) =  
   let ctx = { HttpContext.empty with request = ToSuaveRequest req}
