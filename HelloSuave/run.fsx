e58a9c728420d0576baa5cc1cce193f0384fcabf
Tamizhvendan S <tamizh88@gmail.com>
2016-09-11 17:41:45 +0530

    Adding Suave

--- HelloSuave/run.fsx
+++ HelloSuave/run.fsx
@@ -1,10 +1,43 @@
-open System.Net
-open System.Net.Http.Headers
 open System
+open System.Net.Http
+open System.Net.Http.Headers
+open Suave.Http
+open Suave
+open Suave.Successful
+open System.Threading
+open System.Threading.Tasks
+open Suave.Operators
+open Suave.Filters
 
-let Run (req: HttpRequestMessage) =
-    let response = new HttpResponseMessage()
-    response.Content <- new StringContent(""" { "hello" : "world!" } """)
-    response.StatusCode <- HttpStatusCode.OK
-    response.Content.Headers.ContentType <- MediaTypeHeaderValue("application/json")
-    response
+let ToSuaveHttpMethod (httpMethod : System.Net.Http.HttpMethod) = 
+  match httpMethod.Method with
+  | "GET" -> HttpMethod.GET
+  | "POST" -> HttpMethod.POST
+  | _ -> HttpMethod.OTHER "unknown"
+
+let ToSuaveRequest (req : HttpRequestMessage) = 
+  {HttpRequest.empty with 
+    url = req.RequestUri
+    ``method`` = ToSuaveHttpMethod req.Method
+    }
+
+let toHttpResponseMessage (httpResult : HttpResult) = 
+  let content = function
+  | Bytes c -> c
+  | _ -> Array.empty
+  let res = new HttpResponseMessage()
+  res.Content <- new ByteArrayContent(content httpResult.content)
+  res
+
+let app = GET >=> OK "hello"
+
+let Run (req : HttpRequestMessage) =  
+  let ctx = { HttpContext.empty with request = ToSuaveRequest req}
+  let res = app ctx |> Async.RunSynchronously
+  match res with
+  | Some ctx ->
+    toHttpResponseMessage ctx.response
+  | _ -> 
+    let res = new HttpResponseMessage()
+    res.Content <- new ByteArrayContent(Array.empty)
+    res
