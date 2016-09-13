cd617d46bea4fe8509a200b82f25a5bf62e81f96
Tamizhvendan S <tamizh88@gmail.com>
2016-09-13 12:04:38 +0530

    unleashing the power of suave

--- HelloSuave/app.fsx
+++ HelloSuave/app.fsx
@@ -5,9 +5,18 @@ open Suave.Operators
 open Suave.Filters
 
 let app = 
-  path "/test2" >=> choose [
-                      GET >=> OK "GET"
-                      POST >=> OK "POST"
-                      PUT >=> OK "PUT"
-                      DELETE >=> OK "DELETE"
-                    ]
+  choose [
+    path "/test2" >=> choose [
+      GET >=> OK "GET test2"
+      POST >=> OK "POST test2"
+      PUT >=> OK "PUT test2"
+      DELETE >=> OK "DELETE test2"
+    ]
+    path "/test" >=> choose [
+      GET >=> OK "GET test"
+      POST >=> OK "POST test"
+      PUT >=> OK "PUT test"
+      DELETE >=> OK "DELETE test"
+    ]
+  ]
+                    
